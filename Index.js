10153194ef5dc17a8992d3f45fcf715779edc470
Antonio-2202 <antoniomar2003@gmail.com>
2024-06-17 20:47:20 +0200

    Exercise #2 ¿Es un anagrama?

--- Index.js
+++ Index.js
@@ -25,4 +25,20 @@ function fizzbuzz() {
     }
 }
 
-fizzbuzz();
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+function isAnAnagram(string1, string2) {
+    if(string1.toLowerCase() === string2.toLowerCase()) {
+        console.log(string1.toLowerCase() === string2.toLowerCase())
+        return false
+    }    
+    
+    return string1.toLowerCase().split('').sort() === string2.toLowerCase().split('').sort()
+}
